ee6bd39ad8a7f26f84ca42ceb93a278ada513cd4
Wilton Reis <wiltonfilho0825@gmail.com>
2025-11-06 12:40:47 -0300

    feat: Adiciona rest template

--- payroll/src/main/java/com/wiltonreis/payroll/services/PaymentService.java
+++ payroll/src/main/java/com/wiltonreis/payroll/services/PaymentService.java
@@ -1,13 +1,27 @@
 package com.wiltonreis.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.wiltonreis.payroll.entities.Payment;
+import com.wiltonreis.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
     
     public Payment getPayment(Long workerId, int days){
-        return new Payment("John Doe", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+
+        Worker worker = new RestTemplate().getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
